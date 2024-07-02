c716c9ed04527fadb92f599f42402c34cff19c95
arandomguydx <162324828+arandomguydx@users.noreply.github.com>
2024-07-02 20:45:42 +0900

    Lighting + movement improvements
    I added the limited vision and fixed some issues with the player and monster movement. I also made it so that u can move with either wasd or the arrow keys as well as the ability to sprint. The items aren't ready yet so ill work on those next. Let me know if u want me to cahnge anything

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,13 +1,30 @@
 extends CharacterBody2D
 
-const speed = 1500
+@export var speed = 1000
+var ogSpeed = speed
+var sprintSpeed = speed*2
 var curse = 0
 
 func _physics_process(delta):
-	var directionX = Input.get_axis("ui_left", "ui_right")
-	var directionY = Input.get_axis("ui_up", "ui_down")
-	velocity.x += directionX * delta * speed
-	velocity.y += directionY * delta * speed
+	
+	if(Input.is_action_pressed("sprint")):
+		speed=sprintSpeed
+	else:
+		speed=ogSpeed
+	
+	if(Input.is_action_pressed("up")):
+		velocity.y = Vector2.UP.y * speed
+	if(Input.is_action_pressed("down")):
+		velocity.y = Vector2.DOWN.y * speed
+	if(Input.is_action_pressed("left")):
+		velocity.x = Vector2.LEFT.x * speed
+	if(Input.is_action_pressed("right")):
+		velocity.x = Vector2.RIGHT.x * speed
+	
+	if(Input.is_action_just_released("up") || Input.is_action_just_released("down")):
+		velocity.y=0 
+	if(Input.is_action_just_released("left") || Input.is_action_just_released("right")):
+		velocity.x=0 
 
 	move_and_slide()
 	
